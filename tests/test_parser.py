76bdc59cc1e5a2d9914f68508d94e7a5ccb228f5
ThreatLead <threatlead@gmail.com>
2018-01-15 14:12:46 -0800

    Updated test cases

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,30 +5,33 @@ import unittest
 
 class ConnectTestSuite(unittest.TestCase):
 
+    def setUp(self):
+        self._connection = hybridanalysis.HybridAnalysis(api_key=API_KEY, api_secret=API_SECRET)
+
     def test_ha_key_data(self):
-        response = hybridanalysis.HybridAnalysis(api_key=API_KEY, api_secret=API_SECRET).api_key_data()
-        self.assertEqual(response['response'], 0, 'Invalid Response for key_data check.')
+        response = self._connection.api_key_data()
+        self.assertEqual(response['response_code'], 0, 'Invalid Response for key_data check.')
 
     def test_ha_quota(self):
-        response = hybridanalysis.HybridAnalysis(api_key=API_KEY, api_secret=API_SECRET).quota()
-        self.assertEqual(response['response'], 0, 'Invalid Response for quota check.')
+        response = self._connection.quota()
+        self.assertEqual(response['response_code'], 0, 'Invalid Response for quota check.')
 
     def test_ha_feed(self):
-        response = hybridanalysis.HybridAnalysis(api_key=API_KEY, api_secret=API_SECRET).feed(days=1)
-        self.assertEqual(response['response'], 0, 'Invalid Response for feed check.')
+        response = self._connection.feed(days=1)
+        self.assertEqual(response['status'], 'ok', 'Invalid Response for feed check.')
 
     def test_ha_limits(self):
-        response = hybridanalysis.HybridAnalysis(api_key=API_KEY, api_secret=API_SECRET).limits()
-        self.assertEqual(response['response'], 0, 'Invalid Response for feed check.')
+        response = self._connection.limits()
+        self.assertEqual(response['response_code'], 0, 'Invalid Response for feed check.')
 
     def test_ha_environments(self):
-        response = hybridanalysis.HybridAnalysis(api_key=API_KEY, api_secret=API_SECRET).environments()
-        self.assertEqual(response['response'], 0, 'Invalid Response for feed check.')
+        response = self._connection.environments()
+        self.assertEqual(response['response_code'], 0, 'Invalid Response for feed check.')
 
     def test_ha_scan(self):
         sha256 = '040c0111aef474d8b7bfa9a7caa0e06b4f1049c7ae8c66611a53fc2599f0b90f'
-        response = hybridanalysis.HybridAnalysis(api_key=API_KEY, api_secret=API_SECRET).scan(sha256=sha256)
-        self.assertEqual(response['response'], 0, 'Invalid Response for scan check.')
+        response = self._connection.scan(sha256=sha256)
+        self.assertEqual(response['response_code'], 0, 'Invalid Response for scan check.')
 
 
 if __name__ == '__main__':
